f1f02b6baa423900fb491f5cac12e69c62a446f3
Vadivelu M <vadivelumsolo@gmail.com>
2026-06-12 02:00:15 +0530

    s3 terraform

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@ terraform {
 
     backend "s3" {
         bucket = "terraform-backend-state"
-        key = "k8s_key/terraform.tfstate"
+        key = "terraform_backend_s3/terraform.tfstate"
         region = "us-east-1"
         encrypt = true
         use_lockfile = true
